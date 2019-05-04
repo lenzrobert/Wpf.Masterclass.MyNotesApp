33d9c00d2fcb607e59d17593cf77c215f9da0782
Robert <lenzrobert@users.noreply.github.com>
2019-05-04 23:22:52 +0200

    Usings sorted and unused removed

--- Wpf.Masterclass.MyNotesApp/ViewModel/LoginViewModel.cs
+++ Wpf.Masterclass.MyNotesApp/ViewModel/LoginViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Wpf.Masterclass.MyNotesApp.Model;
+﻿using Wpf.Masterclass.MyNotesApp.Model;
 
 namespace Wpf.Masterclass.MyNotesApp.ViewModel
 {
